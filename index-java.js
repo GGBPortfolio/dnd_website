e5d1ed9a79013479001c3103fe3c2e56a5a5cb1d
GGBPortfolio <4everdming@gmail.com>
2026-02-04 10:09:46 -0600

    connecting each button to bring user to the respective html pages + fixed bugs

--- index-java.js
+++ index-java.js
@@ -1,6 +1,21 @@
 
 document.getElementById('sessionButton').addEventListener("click",(sessionPage));
+document.getElementById('mapButton').addEventListener("click",(mapPage));
+document.getElementById('artButton').addEventListener("click",(artPage));
+document.getElementById('loreButton').addEventListener("click",(lorePage));
 
 function sessionPage() { 
     window.location.href = "recaps.html";
+}
+
+function mapPage(){
+     window.location.href = "map.html"
+}
+
+function artPage(){
+     window.location.href = "art.html"
+}
+
+function lorePage(){
+     window.location.href = "lore.html"
 }
