206552b67d537cf927d63a59b56c519b5ebe72ee
GGBPortfolio <4everdming@gmail.com>
2026-02-10 11:19:52 -0600

    stylizing the flex containers + fixing flex display to show proper grid inside bordered box

--- art-java.js
+++ art-java.js
@@ -3,14 +3,12 @@ document.getElementById('prevButton').addEventListener("click",(prevImage));
 
 
 
-const imageMapArray = ["images/maps/First Impression Tavern.png", "images/maps/Campfire 1.png", "images/maps/Campfire 2.png", 
-    "images/maps/Road 1 (copy).png", "images/maps/road 2 (copy).png", "images/maps/Olburr Sootfoot big.png",
-    "images/maps/Demonic Sewer.png", "images/maps/Cpt Drum Big.png", "images/maps/Kobold Den (Underdark Tunnel Entrance).png",
-    "images/maps/Druid Auction.png", "images/maps/Druid Auction FL 2.png", "images/maps/Crab Fungal Cave.png",
-    "images/maps/Crab Fungal Cave p1.png", "images/maps/Pirate Shipwreck.png", "images/maps/Rice fields.png",
-    "images/maps/Toll House Ambush.png", "images/maps/Petty Pistol Bar.png", "images/maps/Oni Trade on the Water.png",
-    "images/maps/Witch's Swamp.png", "images/maps/Sewer 4.png", "images/maps/Sewer Dundal - RP.png", "images/maps/Costal Ruins.png", 
-    "images/maps/Temple of Cercie (1).png"]
+const imageMapArray = ["images/maps/First Impression Tavern.png", "images/maps/Campfire 1.png", "images/maps/Campfire 2.png", "images/maps/Road 1 (copy).png", 
+    "images/maps/road 2 (copy).png", "images/maps/Olburr Sootfoot big.png","images/maps/Demonic Sewer.png", "images/maps/Cpt Drum Big.png", 
+    "images/maps/Kobold Den (Underdark Tunnel Entrance).png", "images/maps/Druid Auction.png", "images/maps/Druid Auction FL 2.png", "images/maps/Crab Fungal Cave.png",
+    "images/maps/Crab Fungal Cave p1.png", "images/maps/Pirate Shipwreck.png", "images/maps/Rice fields.png", "images/maps/Toll House Ambush.png", 
+    "images/maps/Petty Pistol Bar.png", "images/maps/Oni Trade on the Water.png", "images/maps/Witch's Swamp.png", "images/maps/Sewer 4.png", 
+    "images/maps/Sewer Dundal - RP.png", "images/maps/Costal Ruins.png", "images/maps/Temple of Cercie (1).png"]
 
 //TILE LIBRARY NEXT TO IT??//
 
@@ -28,6 +26,7 @@ function nextImage(){
     else {
         currentIndex++;
         myImage.src = imageMapArray[currentIndex];
+        console.log(listSizeMaps)
     }
     
 }
