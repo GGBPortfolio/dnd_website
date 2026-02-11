552057e4fc504a071c3c8857b48487be7c6157f0
GGBPortfolio <4everdming@gmail.com>
2026-02-11 09:46:31 -0600

    added descriptive sentences to each map + fixed functions so that the image matched the descriptions

--- art-java.js
+++ art-java.js
@@ -8,25 +8,43 @@ const imageMapArray = ["images/maps/First Impression Tavern.png", "images/maps/C
     "images/maps/Kobold Den (Underdark Tunnel Entrance).png", "images/maps/Druid Auction.png", "images/maps/Druid Auction FL 2.png", "images/maps/Crab Fungal Cave.png",
     "images/maps/Crab Fungal Cave p1.png", "images/maps/Pirate Shipwreck.png", "images/maps/Rice fields.png", "images/maps/Toll House Ambush.png", 
     "images/maps/Petty Pistol Bar.png", "images/maps/Oni Trade on the Water.png", "images/maps/Witch's Swamp.png", "images/maps/Sewer 4.png", 
-    "images/maps/Sewer Dundal - RP.png", "images/maps/Costal Ruins.png", "images/maps/Temple of Cercie (1).png"]
+    "images/maps/Sewer Dundal - RP.png","images/maps/Costal Ruins.png", "images/maps/Feywild Ballroom with Golden Throne.png", "images/maps/Temple of Cercie (1).png"]
 
-//TILE LIBRARY NEXT TO IT??//
+const mapSentences = ["This is the First Impression Tavern, where our heros' journey began.", "This is one of many travel assets prepared in case of random encounter confrontations (Campfire 1)", "This is one of many travel assets prepared in case of random encounter confrontations (Campfire 2).",
+    "This is one of many travel assets prepared in case of random encounter confrontations (Road 1).", "This is one of many travel assets prepared in case of random encounter confrontations (Road 2).",
+    "The Boys encountered a roadblock set up by Olburr Sootfoot and his pack of bandits.", "(UNUSED) This is part of the sewer system in Circos, where a group of Oni Clan fiends have been abducting children.",
+    "(UNUSED) A small abondoned port town close to Circos where the legendary pirate captain of the Bonfire Pirates, Drum Takran, was holding up.", "(UNUSED) The entrance to Ahanjoo Clan ruins has become the home to a giant pack of kobolds.",
+    "(UNUSED) A remote mansion north of Circos where an illegal auction of Druid children was taking place (Floor 1).", "(UNUSED) A remote mansion north of Circos where an illegal auction of Druid children was taking place (Floor 2).",
+    "(UNUSED) Bullder Ross, the traveling alchemist from Barronia that the party met, was in need of a specific mushroom from these crab infested caves.", "(UNUSED) The deeper part of the crab cave system where sea-worshipping cultists were preparing a sacrifice.",
+    "This is the shipwreck where the party did battle against the Plaguebringers alongside Emiko Fireforge. Here they found the cursed sword that would be Hack Sparrow's demise.", "(UNUSED) A rice-field village being attacked by a group of summoned demons.", 
+    "(UNUSED) At the toll house the party stopped at, the Enya Clan would offer coin in exchange for helping them steal back the food taken from them by the crown.", "(UNUSED) If the party decided to work for the Oni Clan in Barronia, their first job would have been to clear out this bar under the control of the Petty Pistol Possy.",
+    "(UNUSED) If the party decided to work for the Humming Oath in Barronia, their first job would have been to intercept this weapons trade between the Red Raven Pirates and the Oni Clan.", "This swamp is where the party followed a group of witches impersonating Lady Myrathel to lure courtisans to their demise.",
+    "This is the part of the Barronia sewer system where the party found the body of Emmo Twoman's brother. Deeper within is the hideout of the Humming Oath faction.", "(UNUSED) Another part of Barronia's sewer system, this being directly underneath the Grand Barrett Palace. This is how the Humming Oath had access to the festivities within.",
+    "These are the costal ruins the party ambushed pirate hunters set to attack Prism Talbot's suppliers, the Red Raven Pirates.", "This is one of the many ballrooms in the Grand Barrett Palace where the party witnessed a three-way skirmish between the big players of the city.", 
+    "(UNUSED) These are the ancient ruins the party found directions for on a silver, Druidic disk. It is an old temple to one of the reincarnated prophets, Cercie the Bountiful."
+]
 
 const listSizeMaps = imageMapArray.length;
 let currentIndex = 0;
-
+let sentence = mapSentences[currentIndex];
 
 document.getElementById('myImage').src = imageMapArray[currentIndex];
+document.getElementById("mapDescription").textContent = sentence;
 
 function nextImage(){
     if (currentIndex + 1 == listSizeMaps){
         currentIndex = 0;
         myImage.src = imageMapArray[currentIndex];
+        sentence = mapSentences[currentIndex];
+        document.getElementById("mapDescription").textContent = sentence; 
+
     }
     else {
         currentIndex++;
         myImage.src = imageMapArray[currentIndex];
         console.log(listSizeMaps)
+        sentence = mapSentences[currentIndex];
+        document.getElementById("mapDescription").textContent = sentence;
     }
     
 }
@@ -34,11 +52,15 @@ function nextImage(){
 function prevImage(){
     if (currentIndex == 0) {
         currentIndex = listSizeMaps - 1;
-        myImage.src = imageMapArray[currentIndex]; 
+        myImage.src = imageMapArray[currentIndex];
+        sentence = mapSentences[currentIndex];
+        document.getElementById("mapDescription").textContent = sentence; 
     }
     else {
         currentIndex--; 
         myImage.src = imageMapArray[currentIndex];
+        sentence = mapSentences[currentIndex];
+        document.getElementById("mapDescription").textContent = sentence;
     }
 
 }
@@ -46,4 +68,6 @@ function prevImage(){
 function mapGridClick (num){
     currentIndex = num;
     myImage.src = imageMapArray[currentIndex];
+    sentence = mapSentences[currentIndex];
+    document.getElementById("mapDescription").textContent = sentence;
 }
