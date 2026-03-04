db3b861d5bee134eda97f85ebd424ee7308350a6
GGBPortfolio <4everdming@gmail.com>
2026-03-04 08:42:54 -0600

    messing around with the 'fetch' mechanic to pull from an array of .txt files and show them on the HTML page

--- recaps-java.js
+++ recaps-java.js
@@ -3,7 +3,15 @@ document.getElementById('mapButton').addEventListener("click",(mapPage));
 document.getElementById('homeButton').addEventListener("click",(homePage));
 document.getElementById('loreButton').addEventListener("click",(lorePage));
 
+const textFiles = ["test.txt", "test2.txt", "teat3.txt"];
+const output = document.getElementById("output");
+let specificSesh = 0;
 
+fetch(textFiles[specificSesh])
+     .then(res => res.text())
+     .then(text => {
+          output.innerHTML = "<h3>" + text + "</h3>";
+     });
 
 
 //TAB BUTTONS//
@@ -12,13 +20,13 @@ function artPage() {
 }
 
 function mapPage(){
-     window.location.href = "map.html"
+     window.location.href = "map.html";
 }
 
 function homePage(){
-     window.location.href = "index.html"
+     window.location.href = "index.html";
 }
 
 function lorePage(){
-     window.location.href = "lore.html"
+     window.location.href = "lore.html";
 }
