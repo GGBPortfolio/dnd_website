4f5bf3c90ee3f0e9d72d5331f803623664202e8a
GGBPortfolio <4everdming@gmail.com>
2026-03-05 08:45:03 -0600

    fixed some styling issues + added to the lore array list

--- lore-java.js
+++ lore-java.js
@@ -3,7 +3,7 @@ document.getElementById('mapButton').addEventListener("click",(mapPage));
 document.getElementById('homeButton').addEventListener("click",(homePage));
 document.getElementById('sessionButton').addEventListener("click",(sessionPage));
 
-const textFiles = [];
+const textFiles = ["Lore/Deco's Backstory.md", "Lore/Gordon's backstory.md", "Lore/Timmy's Backstory.md", "Lore/Jake Backstory First Draft.md", "Lore/James' Backstory.md", "Lore/Mark's Backstory.md", "Lore/Pierce Backstory.md"];
 
 const output = document.getElementById("output");
 let specificSesh = 0;
