91e689958c9e6768f70bc265bc4140afdf6e6d51
GGBPortfolio <4everdming@gmail.com>
2026-03-05 07:35:43 -0600

    copied the recap functions + styling to the lore files

---
+++ lore-java.js
@@ -0,0 +1,41 @@
+document.getElementById('artButton').addEventListener("click",(artPage));
+document.getElementById('mapButton').addEventListener("click",(mapPage));
+document.getElementById('homeButton').addEventListener("click",(homePage));
+document.getElementById('sessionButton').addEventListener("click",(sessionPage));
+
+const textFiles = [];
+
+const output = document.getElementById("output");
+let specificSesh = 0;
+
+fetch(textFiles[specificSesh])
+     .then(res => res.text())
+     .then(text => {
+          output.innerHTML = "<p>" + text + "</p>";
+     });
+
+   function loreGridClick(num){
+     fetch(textFiles[num])
+     .then(res => res.text())
+     .then(text => {
+          output.innerHTML = "<p>" + text + "</p>";
+     });
+   }  
+
+
+//TAB BUTTONS//
+function artPage() { 
+    window.location.href = "art.html";
+}
+
+function mapPage(){
+     window.location.href = "map.html";
+}
+
+function homePage(){
+     window.location.href = "index.html";
+}
+
+function sessionPage(){
+     window.location.href = "recaps.html";
+}
