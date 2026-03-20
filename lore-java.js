2e00c64192e74a4eafc48b3856ebf8ef4f8f81ac
GGBPortfolio <4everdming@gmail.com>
2026-03-20 09:13:10 -0500

    recreated the button function from the other two text sections for the LETTERS text box

--- lore-java.js
+++ lore-java.js
@@ -4,9 +4,11 @@ document.getElementById('homeButton').addEventListener("click",(homePage));
 document.getElementById('sessionButton').addEventListener("click",(sessionPage));
 
 const textFiles = ["Lore/Deco's Backstory.md", "Lore/Gordon's backstory.md", "Lore/Timmy's Backstory.md", "Lore/Jake Backstory First Draft.md", "Lore/James' Backstory.md", "Lore/Mark's Backstory.md", "Lore/Pierce Backstory.md", "Lore/SemajDream.md", "Lore/AngussDream.md", "Lore/HackDream.md", "Lore/MordosDream.md", "Lore/TimbleDream.md", "Lore/VeyronDream.md", "Lore/ZellicekDream.md", "Lore/A Message from Glassya.md", "Lore/newGlasyaMessage.md",
-     "Lore/Copy of From Kids To Kings EDITOR.md", "Lore/Town Crier News BARRONIA.md", "Lore/ROYAL DECREE_ BARRONIA .md", "Lore/Coronation QnA.md", "Lore/The Mirror Wings.md", "Lore/Grand Barrett Palace_ Emergency Court Meeting.md", "Lore/Gavyn and Einar Meeting_ PALOMERAS.md", "Lore/Einar x Brozzerious_ Marble City.md"
+     "Lore/Copy of From Kids To Kings EDITOR.md", "Lore/Town Crier News BARRONIA.md", "Lore/ROYAL DECREE_ BARRONIA .md", "Lore/Coronation QnA.md", "Lore/The Mirror Wings.md", "Lore/Grand Barrett Palace_ Emergency Court Meeting.md", "Lore/Gavyn and Einar Meeting_ PALOMERAS.md", "Lore/Einar x Brozzerious_ Marble City.md",
+     "Lore/BOOK Excerpt_ Druid Nations.md", "Lore/BOOK Excerpt_ Einar.md"
 ];
 
+//Page will load the Dreams section//
 const output = document.getElementById("output");
 let specificSesh = 0;
 
@@ -16,6 +18,7 @@ fetch(textFiles[specificSesh])
           output.innerHTML = "<p>" + text + "</p>";
      });
 
+//Page will load the Events section//     
 const output2 = document.getElementById("output2");
 let specificSesh2 = 16;
 
@@ -23,6 +26,16 @@ fetch(textFiles[specificSesh2])
      .then(res => res.text())
      .then(text => {
           document.getElementById("output2").innerHTML = marked.parse(text);
+     });
+
+//Page will load the Letters section//
+const output3 = document.getElementById("output3");
+let specificSesh3 = 24;
+
+fetch(textFiles[specificSesh3])
+     .then(res => res.text())
+     .then(text => {
+          document.getElementById("output3").innerHTML = marked.parse(text);
      });
 
 
@@ -52,9 +65,18 @@ function newGridClick2(num){
      document.getElementById("output2").innerHTML = marked.parse(text);
   }); 
 }
+
+//LETTERS & DOCUMENTS//
+function newGridClick3(num){
+     fetch(textFiles[num])
+     .then(response => response.text())
+     .then(text => {
+     document.getElementById("output3").innerHTML = marked.parse(text);
+  }); 
+}
    
 
-///TESTING TO MOVE LATER///
+///NO LONGER IN USE///
    fetch("Lore/CROWN Charts & MAGIC SHOPPE.md")
   .then(response => response.text())
   .then(text => {
